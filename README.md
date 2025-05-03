2700c04c8116b09b71b33c1ec744074390362730
Justin Dray <justin@dray.be>
2025-05-04 01:43:26 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,33 +1,14 @@
-# haaska: Home Assistant Alexa Skill Adapter
+# Welcome to your CDK TypeScript project
 
-[![Main](https://github.com/mike-grant/haaska/actions/workflows/main.yml/badge.svg)](https://github.com/mike-grant/haaska/actions/workflows/main.yml)
+This is a blank project for CDK development with TypeScript.
 
----
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
 
-haaska implements a bridge between the [Home Assistant Smart Home API](https://www.home-assistant.io/components/alexa/#smart-home) and the [Alexa Smart Home Skill API](https://developer.amazon.com/alexa/smart-home) from Amazon.
+## Useful commands
 
-This provides voice control for a connected home managed by Home Assistant, through any Alexa-enabled device.
-
-### Getting Started
-To get started, head over to the [haaska Wiki](https://github.com/mike-grant/haaska/wiki).
-
-### Development
-
-Run tests
-
-```
-python -m pytest test.py
-```
-
-### Thanks and Acknowledgement
-
-Thanks to [@auchter](https://github.com/auchter) for creating the original haaska.
-
-Thanks to [@bitglue](https://github.com/bitglue) for his work in getting the Smart Home API exposed via HTTP, making this slimmed down version possible.
-
-This fork of haaska was created by [@mike-grant](https://github.com/mike-grant).
-
-Documentation and additional maintenance is done by [@anthonylavado](https://github.com/anthonylavado), and contributors like you.
-
-### License
-haaska is provided under the [MIT License](LICENSE).
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `npx cdk deploy`  deploy this stack to your default AWS account/region
+* `npx cdk diff`    compare deployed stack with current state
+* `npx cdk synth`   emits the synthesized CloudFormation template
--- src/haaska.py
+++ src/haaska.py
@@ -21,9 +21,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import os
 import json
 import logging
+import os
+
 import requests
 
 logger = logging.getLogger()
@@ -35,15 +36,15 @@ class HomeAssistant(object):
 
         self.session = requests.Session()
         self.session.headers = {
-            'Authorization': f'Bearer {config.bearer_token}',
-            'content-type': 'application/json',
-            'User-Agent': self.get_user_agent()
+            "Authorization": f"Bearer {config.bearer_token}",
+            "content-type": "application/json",
+            "User-Agent": self.get_user_agent(),
         }
         self.session.verify = config.ssl_verify
         self.session.cert = tuple(config.ssl_client)
 
     def build_url(self, endpoint):
-        return f'{self.config.url}/api/{endpoint}'
+        return f"{self.config.url}/api/{endpoint}"
 
     def get_user_agent(self):
         library = "Home Assistant Alexa Smart Home Skill"
@@ -59,16 +60,17 @@ class HomeAssistant(object):
     def post(self, endpoint, data, wait=False):
         read_timeout = None if wait else 0.01
         try:
-            logger.debug(f'calling {endpoint} with {data}')
-            r = self.session.post(self.build_url(endpoint),
-                                  data=json.dumps(data),
-                                  timeout=(None, read_timeout))
+            logger.debug(f"calling {endpoint} with {data}")
+            r = self.session.post(
+                self.build_url(endpoint),
+                data=json.dumps(data),
+                timeout=(None, read_timeout),
+            )
             r.raise_for_status()
             return r.json()
         except requests.exceptions.ReadTimeout:
             # Allow response timeouts after request was sent
-            logger.debug(
-                f'request for {endpoint} sent without waiting for response')
+            logger.debug(f"request for {endpoint} sent without waiting for response")
             return None
 
 
@@ -82,11 +84,11 @@ class Configuration(object):
         if opts_dict is not None:
             self._json = opts_dict
 
-        self.url = self.get_url(self.get(['url', 'ha_url']))
-        self.ssl_verify = self.get(['ssl_verify', 'ha_cert'], default=True)
-        self.bearer_token = self.get(['bearer_token'], default='')
-        self.ssl_client = self.get(['ssl_client'], default='')
-        self.debug = self.get(['debug'], default=False)
+        self.url = self.get_url(self.get(["url", "ha_url"]))
+        self.ssl_verify = self.get(["ssl_verify", "ha_cert"], default=True)
+        self.bearer_token = self.get(["bearer_token"], default="")
+        self.ssl_client = self.get(["ssl_client"], default="")
+        self.debug = self.get(["debug"], default=False)
 
     def get(self, keys, default=None):
         for key in keys:
@@ -103,9 +105,9 @@ class Configuration(object):
 
 
 def event_handler(event, context):
-    config = Configuration('config.json')
+    config = Configuration("config.json")
     if config.debug:
         logger.setLevel(logging.DEBUG)
     ha = HomeAssistant(config)
 
-    return ha.post('alexa/smart_home', event, wait=True)
+    return ha.post("alexa/smart_home", event, wait=True)
